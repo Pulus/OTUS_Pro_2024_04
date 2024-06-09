17ef0383de2c855d02753e15337502c6c51378fe
Pulus <kaurovpavel@gmail.com>
2024-06-09 14:53:07 +0300

    hw04-generics Homework for verification

--- hw04-generics/src/main/java/homework/CustomerService.java
+++ hw04-generics/src/main/java/homework/CustomerService.java
@@ -1,21 +1,58 @@
 package homework;
 
-import java.util.Map;
+import java.util.Comparator;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
-@SuppressWarnings({"java:S1186", "java:S1135", "java:S1172"}) // при выполнении ДЗ эту аннотацию надо удалить
 public class CustomerService {
 
-    // todo: 3. надо реализовать методы этого класса
-    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final TreeMap<Customer, String> map = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
-    public Map.Entry<Customer, String> getSmallest() {
-        // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+    public Entry<Customer, String> getSmallest() {
+        return new Entry<>() {
+            @Override
+            public Customer getKey() {
+                Customer nwSmallest = map.firstEntry().getKey();
+                return new Customer(nwSmallest.getId(), nwSmallest.getName(), nwSmallest.getScores());
+            }
+
+            @Override
+            public String getValue() {
+                return map.firstEntry().getValue();
+            }
+
+            @Override
+            public String setValue(String value) {
+                return map.firstEntry().setValue(value);
+            }
+        };
     }
 
-    public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+    public Entry<Customer, String> getNext(Customer customer) {
+        if (map.higherEntry(customer) != null) {
+            return new Entry<>() {
+                @Override
+                public Customer getKey() {
+                    Customer nwBiggest = map.higherEntry(customer).getKey();
+                    return new Customer(nwBiggest.getId(), nwBiggest.getName(), nwBiggest.getScores());
+                }
+
+                @Override
+                public String getValue() {
+                    return map.higherEntry(customer).getValue();
+                }
+
+                @Override
+                public String setValue(String value) {
+                    return map.higherEntry(customer).setValue(value);
+                }
+            };
+        } else {
+            return null;
+        }
     }
 
-    public void add(Customer customer, String data) {}
+    public void add(Customer customer, String data) {
+        map.put(customer, data);
+    }
 }
