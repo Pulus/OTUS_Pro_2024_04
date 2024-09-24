1de6d8fa56e1b6fcff11c858c7a6b0908a4531ba
Pulus <kaurovpavel@gmail.com>
2024-09-24 16:59:35 +0300

    hw21- HW for verification

--- hw21-jpql/src/main/java/ru/otus/crm/model/Client.java
+++ hw21-jpql/src/main/java/ru/otus/crm/model/Client.java
@@ -5,6 +5,8 @@ import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Getter
 @Setter
@@ -22,6 +24,14 @@ public class Client implements Cloneable {
     @Column(name = "name")
     private String name;
 
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JoinColumn(name = "client_id", nullable = false, updatable = false, insertable = false)
+    @Fetch(FetchMode.JOIN)
+    private List<Phone> phones;
+
     public Client(String name) {
         this.id = null;
         this.name = name;
@@ -33,17 +43,25 @@ public class Client implements Cloneable {
     }
 
     public <E> Client(Long id, String name, Address address, List<Phone> phones) {
-        throw new UnsupportedOperationException();
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phones;
+        this.phones.forEach(phone -> phone.setClient(this));
     }
 
     @Override
     @SuppressWarnings({"java:S2975", "java:S1182"})
     public Client clone() {
-        return new Client(this.id, this.name);
+        return new Client(
+                this.id,
+                this.name,
+                address.clone(),
+                phones.stream().map(Phone::clone).toList());
     }
 
     @Override
     public String toString() {
-        return "Client{" + "id=" + id + ", name='" + name + '\'' + '}';
+        return "Client{" + "id=" + id + ", name='" + name + '\'' + ", address=" + address + ", phones=" + phones + '}';
     }
 }
