d13dcd552e24fd39a9ff32cf1021a9303f1c6673
Pulus <kaurovpavel@gmail.com>
2024-11-11 22:00:17 +0300

    hw25 - Homework for verification

--- hw25-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw25-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,9 +1,11 @@
 package ru.otus.appcontainer;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import org.reflections.Reflections;
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
@@ -17,9 +19,39 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
-        checkConfigClass(configClass);
-        // You code here...
+    private void processConfig(Class<?>... initialConfigClass) {
+        List<Class<?>> classes = sortedConfigByOrder(initialConfigClass);
+
+        for (var configClass : classes) {
+            checkConfigClass(configClass);
+
+            try {
+                Object instance = getInstance(configClass);
+
+                Method[] methods = configClass.getMethods();
+                List<Method> collect = sortedMethodsByOrder(methods);
+
+                for (var m : collect) {
+                    Class<?>[] parameters = m.getParameterTypes();
+                    AppComponent annotation = m.getAnnotation(AppComponent.class);
+
+                    boolean containedValue = appComponentsByName.containsValue(annotation.name());
+                    if (containedValue) {
+                        throw new RuntimeException("Бин с таким параметром уже есть!");
+                    }
+
+                    Object[] args = getArgsForBean(parameters);
+
+                    Object bean = m.invoke(instance, args);
+                    addBean(bean, annotation.name());
+                }
+            } catch (NoSuchMethodException
+                    | InvocationTargetException
+                    | InstantiationException
+                    | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -30,11 +62,60 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        List<Object> collect =
+                appComponents.stream().filter(componentClass::isInstance).toList();
+
+        if (collect.isEmpty()) {
+            throw new RuntimeException("Запрашиваемый бин не найден");
+        } else if (collect.size() != 1) {
+            throw new RuntimeException("Дубликат бинов");
+        } else {
+            return (C) collect.get(0);
+        }
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        Object bean = appComponentsByName.get(componentName);
+
+        if (bean == null) {
+            throw new RuntimeException("Запрашиваемый бин не найден");
+        }
+
+        return (C) bean;
+    }
+
+    private Object getInstance(Class<?> configClass)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        Constructor<?> constructor = configClass.getConstructor();
+        return constructor.newInstance();
+    }
+
+    private List<Method> sortedMethodsByOrder(Method[] methods) {
+        return Arrays.stream(methods)
+                .filter(m -> m.isAnnotationPresent(AppComponent.class))
+                .sorted((Comparator.comparingInt(
+                        o -> o.getDeclaredAnnotation(AppComponent.class).order())))
+                .toList();
+    }
+
+    private List<Class<?>> sortedConfigByOrder(Class<?>[] configClass) {
+        return Arrays.stream(configClass)
+                .sorted((Comparator.comparingInt(o -> o.getDeclaredAnnotation(AppComponentsContainerConfig.class)
+                        .order())))
+                .toList();
+    }
+
+    private void addBean(Object bean, String annotationName) {
+        appComponents.add(bean);
+        appComponentsByName.put(annotationName, bean);
+    }
+
+    private Object[] getArgsForBean(Class<?>[] parameters) {
+        List<Object> args = new ArrayList<>();
+
+        Arrays.stream(parameters).forEach(p -> args.add(getAppComponent(p)));
+
+        return args.toArray();
     }
 }
